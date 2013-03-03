605635cd4e0739e6b0b75cf499053643581034f7
roskvist <k.roskvist@gmail.com>
2013-03-03 21:27:52 +0100

    Initial Commit

---
+++ ForceTap/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ForceTap
+//
+//  Created by Kasper Andersen on 03/03/13.
+//  Copyright (c) 2013 Kasper Andersen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ForceTap/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ForceTap
+//
+//  Created by Kasper Andersen on 03/03/13.
+//  Copyright (c) 2013 Kasper Andersen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
